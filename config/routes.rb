070affb0c94d7c00a785b7ae6612a93cdf1fe0c0
Sean Brown <seanmbrown21@gmail.com>
2014-09-30 20:22:49 -0400

    first bit of work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 WineReview::Application.routes.draw do
+  get "/wine" => "wine#index", as: 'wine'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
