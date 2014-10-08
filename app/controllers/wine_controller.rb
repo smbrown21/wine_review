7fa036c06e23f77a6832c1a578757e7cf656f3cf
Sean Brown <seanmbrown21@gmail.com>
2014-10-07 20:23:48 -0400

    drying out code

--- app/controllers/wine_controller.rb
+++ app/controllers/wine_controller.rb
@@ -1,11 +1,13 @@
 class WineController < ApplicationController
+	before_action :set_wine, only: [ :show, :edit, :update, :destroy]	
+
 	def index
 		@available_at = Time.now
 		@wine = Wine.all
 	end
 
 	def show
-		@wine = Wine.find(params[:id])
+		
 	end
 
 	def new 
@@ -19,16 +21,16 @@ class WineController < ApplicationController
 	end
 
 	def edit
-		@wine = Wine.find(params[:id])
+		
 	end
 	def update
-		@wine = Wine.find(params[:id])
+		
 		@wine.update(wine_params)
 		redirect_to @wine
 	end
 	
 	def destroy
-		@wine = Wine.find(params[:id])
+		
 		@wine.destroy
 		redirect_to wines_url
 	end
@@ -39,4 +41,7 @@ class WineController < ApplicationController
 		params.require(:wine).permit(:name, :year, :winery, :country, :varietal)
 	end
 
+	def set_wine
+		@wine = Wine.find(params[:id])
+	end
 end
