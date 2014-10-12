dcd7e3ac425e66efbea9f4b045f18971a497536c
Sean Brown <seanmbrown21@gmail.com>
2014-10-12 15:24:31 -0400

    Varietals is a constant and updated validation

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,8 +1,11 @@
 class Wine < ActiveRecord::Base
+VARIETALS = ['Chardonnay','Pinot Grigio','Merlot','Sauvignon Blanc','Shiraz Viognier', 'Pinot', 'Mascato','Pinot Noir','Chianti']
 validates :name, :year, :winery, :country, :varietal, presence: true
 validates :year,
 		numericality: { only_integer: true, greater_than_or_equal_to: 1800 },
 		unless: "year.blank?"
 		#add one for varietal
+validates :varietal, inclusion: { in: VARIETALS,
+message: "%{value} is not a valid varietal"}
 
 end
